52899993185e2829be6fbb5e31dd8e96df27ea17
yoyogesh01 <yoyogesh01@gmail.com>
2020-09-10 10:33:48 +0000

    Sync LeetCode submission - Decompress Run-Length Encoded List (c)

---
+++ problems/decompress_run-length_encoded_list/solution.c
@@ -0,0 +1,28 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* decompressRLElist(int* nums, int numsSize, int* returnSize){
+    
+    int *list = (int *)malloc(sizeof(int));
+    
+    int freq = 0, val = 0;
+    int size = 0;
+    for(int i=0; i<numsSize; i=i+2)
+    {
+        freq = nums[i];
+        val = nums[i+1];
+        
+        list = realloc(list, (size + freq)*sizeof(int));
+        
+        for (int j = size; j< (size + freq); j++)
+        {
+            list[j] = val;
+        }
+        size = size + freq;    
+    }
+    
+    *returnSize = size;
+    return list;
+}
