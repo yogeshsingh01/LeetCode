f96bee41d5e923eaa616116b13b95a30403d1319
yoyogesh01 <yoyogesh01@gmail.com>
2020-09-22 20:42:33 +0530

    Added solution for 0002

---
+++ C_Language/0002_add_two_numbers.c
@@ -0,0 +1,44 @@
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+ * *****************************************************************************/
+/**
+ * Definition for singly-linked list.*/
+ struct ListNode {
+     int val;
+     struct ListNode *next;
+ };
+
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    
+    struct ListNode *sum = (struct ListNode *)malloc(sizeof(struct ListNode));
+    struct ListNode *backup = sum;
+    sum->next = NULL;
+    int carry = 0;
+    
+    int val1,val2;
+    while((l1 != NULL) || (l2 != NULL) || carry)
+    {
+        sum->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+        sum = sum->next;    
+        val1 = val2 = 0;
+        
+        if(l1)
+            val1 = l1->val;
+        if(l2)
+            val2 = l2->val;
+        sum->val = (val1 + val2 + carry)%10;
+        carry = (val1 + val2 + carry)/10;
+        if(l1)
+            l1 = l1->next;
+        if(l2)
+            l2 = l2->next;
+        
+    }
+    
+    sum ->next = NULL;
+    return backup->next;
+}
