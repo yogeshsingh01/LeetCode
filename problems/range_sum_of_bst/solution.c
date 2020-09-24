57edb4d73340ed06cef3fb5bdac9743e04c1551e
yoyogesh01 <yoyogesh01@gmail.com>
2020-09-24 11:11:29 +0000

    Sync LeetCode submission - Range Sum of BST (c)

---
+++ problems/range_sum_of_bst/solution.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int rangeSumBST(struct TreeNode* root, int L, int R){
+    
+    if(!root )
+    {
+        return 0;
+    }
+    if(root->val > R)
+    {
+        return rangeSumBST(root->left, L, R);   
+    }
+    
+    if(root->val < L)
+    {
+        return rangeSumBST(root->right, L, R);   
+    }
+    
+    return root->val + rangeSumBST(root->right, L, R) + rangeSumBST(root->left, L, R);
+    
+
+}
